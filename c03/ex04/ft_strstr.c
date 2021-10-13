634996f69af47fbc3456a7f34bf8cc45919ff79c
monk <2012.monk@gmail.com>
2021-10-13 15:57:33 +0900

    c03

--- c03/ex04/ft_strstr.c
+++ c03/ex04/ft_strstr.c
@@ -1,15 +1,14 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strcmp.c                                        :+:      :+:    :+:   */
+/*   ft_strstr.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: seounlee <seounlee@student.42seoul.>       +#+  +:+       +#+        */
+/*   By: seounlee <seounlee@student.42seoul.kr      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/10/12 11:15:38 by seounlee          #+#    #+#             */
-/*   Updated: 2021/10/12 11:15:40 by seounlee         ###   ########.fr       */
+/*   Created: 2021/10/13 14:59:52 by seounlee          #+#    #+#             */
+/*   Updated: 2021/10/13 14:59:53 by seounlee         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
 int	ft_strcmp(char *s1, char *s2)
 {
 	int	result;
@@ -24,4 +23,26 @@ int	ft_strcmp(char *s1, char *s2)
 	if (*s2)
 		result -= *s2;
 	return (result);
+}
+
+char	*ft_strstr(char *str, char *to_find)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	j = 0;
+	if (!*to_find)
+		return (str);
+	while (str[i] && to_find[j])
+	{
+		if (str[i] == to_find[j])
+			j++;
+		else
+			j = str[i] == to_find[0];
+		i++;
+	}
+	if (!to_find[j])
+		return (str + i - j);
+	return (0);
 }
