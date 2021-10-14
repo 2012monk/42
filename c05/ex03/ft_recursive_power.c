82ee190736cba0137f8310ae978f174b9c427c2e
Seoungmook Lee <seounlee@c2r1s4.42seoul.kr>
2021-10-14 21:26:39 +0900

    c05 FIX

--- c05/ex03/ft_recursive_power.c
+++ c05/ex03/ft_recursive_power.c
@@ -12,15 +12,9 @@
 
 int	ft_recursive_power(int nb, int power)
 {
-	int	ret;
-
 	if (power < 0)
 		return (0);
 	if (!power)
 		return (1);
-	ret = ft_recursive_power(nb, power / 2);
-	ret *= ret;
-	if (power & 1)
-		ret *= nb;
-	return (ret);
+	return (ft_recursive_power(nb, power - 1) * nb);
 }
