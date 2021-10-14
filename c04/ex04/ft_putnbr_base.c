463fe560252cc4e736d12199d428151ae47762d8
Seoungmook Lee <seounlee@c2r1s4.42seoul.kr>
2021-10-14 13:27:30 +0900

    c04 submit

--- c04/ex04/ft_putnbr_base.c
+++ c04/ex04/ft_putnbr_base.c
@@ -73,26 +73,3 @@ void	ft_putnbr_base(int nb, char *base)
 		return ;
 	convert_to_base(nb, base, radix);
 }
-
-// int main(int argc, char const *argv[])
-// {
-// 	int t1 = -__INT_MAX__ - 1;
-// 	int t2 = __INT_MAX__;
-// 	int t3 = 0;
-// 	int t4 = 1;
-
-// 	ft_putnbr_base(t3, "0123456789abcdef");
-// 	printf("\n%x", t3);
-// 	ft_putnbr_base(1234567890, "0123456789");
-// 	write(1, "\n", 1);
-// 	ft_putnbr_base(65536, "01");
-// 	write(1, "\n", 1);
-// 	ft_putnbr_base(100000000, "0123456789ABCDEF");
-// 	write(1, "\n", 1);
-// 	ft_putnbr_base(2147438647, "0123456");
-// 	write(1, "\n", 1);
-// 	ft_putnbr_base(-2147438648, "01234");
-// 	write(1, "\n", 1);
-// 	ft_putnbr_base(2147438647, "0123456789ABCDEFGHIJKLMNOPQ");
-// 	return 0;
-// }
