8c9294867ecb7aa6578e9aae376458ec1ed20d5d
monk <2012.monk@gmail.com>
2021-10-18 15:00:11 +0900

    rush01 document

--- Rush01/ex00/run.sh
+++
@@ -1,2 +0,0 @@
-python3 test_gen.py | xargs ./rush > result
-diff -u test_case_result result 
--- Rush01/refactor.c
+++
@@ -1,6 +0,0 @@
-#include <stdlib.h>
-#include <stdio.h>
-#include <unistd.h>
-
-int grid[81];
-int peer[81][16];
